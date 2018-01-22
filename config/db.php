88df2a824136ccb95a1a4c22c3a99022e7864092
Juan Pablo Ibañez Maians <juan.ibanez@outlook.com>
2018-01-22 10:52:58 -0400

    Se creo la conexion a la base de datos ubicada en el servidor induccion.org

--- config/db.php
+++ config/db.php
@@ -2,13 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=gestiontalento',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=induccion.org;dbname=induccio_talento',
+    'username' => 'induccio_juanpa',
+    'password' => 'Rrhh3d_2016',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+        // Schema cache options (for production environment)
+        //'enableSchemaCache' => true,
+        //'schemaCacheDuration' => 60,
+        //'schemaCache' => 'cache',
 ];
