c490661b1984b108f91185805fbd5b6da5291626
Juan Pablo Ibañez Maians <juan.ibanez@outlook.com>
2018-01-22 10:09:33 -0400

    Cambio en el formulario de colabordor se agrega el comentario weeeena!

--- views/colaborador/_form.php
+++ views/colaborador/_form.php
@@ -5,7 +5,7 @@ use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Colaborador */
-/* @var $form yii\widgets\ActiveForm */
+/* @var $form yii\widgets\ActiveForm weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeena*/ 
 ?>
 
 <div class="colaborador-form">
