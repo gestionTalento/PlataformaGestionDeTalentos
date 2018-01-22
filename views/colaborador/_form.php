119fe54b253c934bb8185a8c021b8dec24d17f44
Juan Pablo Ibañez Maians <juan.ibanez@outlook.com>
2018-01-22 10:19:54 -0400

    Le quite el wena

--- views/colaborador/_form.php
+++ views/colaborador/_form.php
@@ -5,7 +5,7 @@ use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Colaborador */
-/* @var $form yii\widgets\ActiveForm weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeena*/ 
+/* @var $form yii\widgets\ActiveForm */ 
 ?>
 
 <div class="colaborador-form">
